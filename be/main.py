4c410a45347cecea0f1b8d5d0b83ba73b72adfd3
kresnajenie <kresna.jenie@gmail.com>
2024-01-29 12:36:33 -0500

    updated main.py
    added cors

--- be/main.py
+++ be/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 import redis_test as re
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Define a list of origins that should be allowed to make requests
+# You can use ["*"] to allow all origins
+origins = [
+    "https://fishies.techkyra.com",
+    "https://kresnajenie.github.io",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,  # Allow credentials (cookies, headers)
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
 
 @app.get("/")
 async def read_root():
