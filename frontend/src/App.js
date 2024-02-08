f899f18ff1f77f64b3793f7e8fd2ffae91417fb9
Serena Chuang <tzyharnchuang@gmail.com>
2024-02-08 14:51:26 -0800

    added button layout (only x, no y, t)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import Navbar from './components/Navbar/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import ButtonLayout from './components/button_layout/button_layout';
 
 function App() {
+  const handleClick = () => {
+    console.log('Button clicked!');
+  };
+
   return (
     <div className="App">
       <Navbar/>
       <p className="test">Hello world</p>
+      <ButtonLayout onClick={handleClick} variant="default">
+        XXXXX
+      </ButtonLayout>
     </div>
   );
 }
